b5c02b7fd662cc68a93a5d89fd1c94c2121f1253
Jan Ulferts <julferts@mesosphere.com>
2018-08-27 16:26:12 +0200

    added associate_public_ip_address

--- main.tf
+++ main.tf
@@ -43,18 +43,19 @@ module "dcos-private-agent-instances" {
     aws = "aws"
   }
 
-  cluster_name       = "${var.cluster_name}"
-  hostname_format    = "${var.hostname_format}"
-  num                = "${var.num_private_agents}"
-  ami                = "${coalesce(var.aws_ami,module.dcos-tested-oses.aws_ami)}"
-  user_data          = "${var.user_data}"
-  instance_type      = "${var.aws_instance_type}"
-  subnet_ids         = ["${var.aws_subnet_ids}"]
-  security_group_ids = ["${var.aws_security_group_ids}"]
-  key_name           = "${var.aws_key_name}"
-  root_volume_size   = "${var.aws_root_volume_size}"
-  root_volume_type   = "${var.aws_root_volume_type}"
-  tags               = "${var.tags}"
+  cluster_name                = "${var.cluster_name}"
+  hostname_format             = "${var.hostname_format}"
+  num                         = "${var.num_private_agents}"
+  ami                         = "${coalesce(var.aws_ami,module.dcos-tested-oses.aws_ami)}"
+  user_data                   = "${var.user_data}"
+  instance_type               = "${var.aws_instance_type}"
+  subnet_ids                  = ["${var.aws_subnet_ids}"]
+  security_group_ids          = ["${var.aws_security_group_ids}"]
+  key_name                    = "${var.aws_key_name}"
+  root_volume_size            = "${var.aws_root_volume_size}"
+  root_volume_type            = "${var.aws_root_volume_type}"
+  associate_public_ip_address = "${var.aws_associate_public_ip_address}"
+  tags                        = "${var.tags}"
 }
 
 resource "null_resource" "private-agents-prereq" {
