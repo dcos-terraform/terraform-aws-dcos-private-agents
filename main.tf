20556c1a3dce43e087c946b5c5fb28ca14053d05
Jan Ulferts <julferts@mesosphere.com>
2018-09-12 19:42:30 +0200

    moved tested oses and prereqs to instance

--- main.tf
+++ main.tf
@@ -23,21 +23,12 @@
 
 provider "aws" {}
 
-module "dcos-tested-oses" {
-  source  = "dcos-terraform/tested-oses/aws"
-  version = "~> 0.0"
-
-  providers = {
-    aws = "aws"
-  }
-
-  os = "${var.dcos_instance_os}"
-}
-
 // Instances is spawning the VMs to be used with DC/OS (bootstrap)
 module "dcos-private-agent-instances" {
-  source  = "dcos-terraform/instance/aws"
-  version = "~> 0.0"
+  # source  = "dcos-terraform/instance/aws"
+  # version = "~> 0.0"
+
+  source = "../terraform-aws-instance"
 
   providers = {
     aws = "aws"
@@ -46,7 +37,7 @@ module "dcos-private-agent-instances" {
   cluster_name                = "${var.cluster_name}"
   hostname_format             = "${var.hostname_format}"
   num                         = "${var.num_private_agents}"
-  ami                         = "${coalesce(var.aws_ami,module.dcos-tested-oses.aws_ami)}"
+  ami                = "${var.aws_ami}"
   user_data                   = "${var.user_data}"
   instance_type               = "${var.aws_instance_type}"
   subnet_ids                  = ["${var.aws_subnet_ids}"]
@@ -56,29 +47,5 @@ module "dcos-private-agent-instances" {
   root_volume_type            = "${var.aws_root_volume_type}"
   associate_public_ip_address = "${var.aws_associate_public_ip_address}"
   tags                        = "${var.tags}"
-}
-
-resource "null_resource" "private-agents-prereq" {
-  // if the user supplies an AMI or user_data we expect the prerequisites are met.
-  count = "${coalesce(var.aws_ami, var.user_data) == "" ? var.num_private_agents : 0}"
-
-  connection {
-    host = "${var.aws_associate_public_ip_address ? element(module.dcos-private-agent-instances.public_ips, count.index) : element(module.dcos-private-agent-instances.private_ips, count.index)}"
-    user = "${module.dcos-tested-oses.user}"
-  }
-
-  provisioner "file" {
-    content = "${module.dcos-tested-oses.os-setup}"
-
-    destination = "/tmp/dcos-prereqs.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/dcos-prereqs.sh",
-      "sudo bash -x /tmp/dcos-prereqs.sh",
-    ]
-  }
-
-  depends_on = ["module.dcos-private-agent-instances"]
+  dcos_instance_os            = "${var.dcos_instance_os}"
 }
--- outputs.tf
+++ outputs.tf
@@ -15,5 +15,10 @@ output "private_ips" {
 
 output "os_user" {
   description = "Output the OS user if default AMI is used"
-  value       = "${module.dcos-tested-oses.user}"
+  value       = "${module.dcos-private-agent-instances.os_user}"
+}
+
+output "prereq-id" {
+  description = "Returns the ID of the prereq script (if user_data or ami are not used)"
+  value       = "${module.dcos-private-agent-instances.prereq-id}"
 }
