8cd5f60b40383aca557aa0cd15766edd110d48c2
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-07-02 14:12:04 +0200

    removed prereq

--- outputs.tf
+++ outputs.tf
@@ -17,8 +17,3 @@ output "os_user" {
   description = "The OS user to be used"
   value       = "${module.dcos-private-agent-instances.os_user}"
 }
-
-output "prereq-id" {
-  description = "Returns the ID of the prereq script (if user_data or ami are not used)"
-  value       = "${module.dcos-private-agent-instances.prereq-id}"
-}
